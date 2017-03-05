5e98134cd0c21c12f8d3ed4f4e54f58430ec2e08
weedz <weedz@users.noreply.github.com>
2017-03-05 22:51:50 +0100

    Use sequelize instead of raw mysql queries

--- server/setup.js
+++ server/setup.js
@@ -1,20 +1,52 @@
-const mysql = require('mysql');
+const Sequelize = require('sequelize');
 const config = require('config');
+const dbConfig = config.get('dbConfig');
+
+const sequelize = new Sequelize(dbConfig.dbName, dbConfig.user, dbConfig.password, {
+    host: dbConfig.host,
+    dialect: dbConfig.type
+});
+
+const News = sequelize.define('news', {
+    title: {
+        type: Sequelize.STRING,
+        field: 'title'
+    },
+    intro: {
+        type: Sequelize.STRING,
+        field: 'intro'
+    },
+    content: {
+        type: Sequelize.STRING,
+        field: 'content'
+    },
+    timestamp: {
+        type: Sequelize.INTEGER,
+        field: 'timestamp'
+    },
+    authorName: {
+        type: Sequelize.STRING,
+        field: 'author_name'
+    },
+    authorId: {
+        type: Sequelize.INTEGER,
+        field: 'author_id'
+    }
+});
+News.sync({
+    force: true
+}).then(() => {
+    News.create({
+        title: 'Article 1',
+        intro: 'Small headline for article 1',
+        content: 'Article body.',
+        authorName: 'Author',
+        authorId: 1
+    });
+});
 
 module.exports = function(app) {
-    const dbConfig = config.get('dbConfig');
-    let params = {
-        user: dbConfig.user,
-        password: dbConfig.password,
-        database: dbConfig.dbName
-    };
-    if (dbConfig.has('socketPath')) {
-        params.socketPath = dbConfig.socketPath;
-    } else {
-        params.host = dbConfig.host;
-        params.port = dbConfig.port;
-    }
-    const connection = mysql.createConnection(params);
-    connection.connect();
-    app.set('mysql', connection);
+    app.set('_db', {
+        News
+    });
 };
